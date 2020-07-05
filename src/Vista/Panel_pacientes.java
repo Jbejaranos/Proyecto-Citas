987f0fb1fffab28ce311b4639770cbbd6c875b8c
Jorge Morales <jomorales@unal.edu.co>
2020-07-05 12:44:39 -0500

    Panel de preguntas frecuentes terminado.

--- src/Vista/Panel_pacientes.java
+++ src/Vista/Panel_pacientes.java
@@ -37,6 +37,7 @@ public class Panel_pacientes extends javax.swing.JPanel {
         Directorio = new javax.swing.JButton();
         registrarFallaBtn = new javax.swing.JButton();
         historiaClinicaBtn = new javax.swing.JButton();
+        preguntasFrecBtn = new javax.swing.JButton();
 
         Panel_variante.setBackground(new java.awt.Color(102, 153, 255));
 
@@ -82,20 +83,30 @@ public class Panel_pacientes extends javax.swing.JPanel {
             }
         });
 
+        preguntasFrecBtn.setText("Preguntas frecuentes");
+        preguntasFrecBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                preguntasFrecBtnActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout Panel_varianteLayout = new javax.swing.GroupLayout(Panel_variante);
         Panel_variante.setLayout(Panel_varianteLayout);
         Panel_varianteLayout.setHorizontalGroup(
             Panel_varianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(Panel_varianteLayout.createSequentialGroup()
                 .addGap(46, 46, 46)
-                .addGroup(Panel_varianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(registrarFallaBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(Panel_varianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(Panel_varianteLayout.createSequentialGroup()
                         .addComponent(B_citas, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(registroCitaButton, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(registroCitaButton, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(Panel_varianteLayout.createSequentialGroup()
+                        .addComponent(registrarFallaBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(Directorio, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(preguntasFrecBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
+                .addGap(18, 18, 18)
+                .addComponent(Directorio, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(B_mis_datos, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -112,9 +123,11 @@ public class Panel_pacientes extends javax.swing.JPanel {
                     .addComponent(B_mis_datos, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(Directorio, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(historiaClinicaBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(39, 39, 39)
-                .addComponent(registrarFallaBtn)
-                .addContainerGap(101, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(Panel_varianteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(registrarFallaBtn, javax.swing.GroupLayout.DEFAULT_SIZE, 104, Short.MAX_VALUE)
+                    .addComponent(preguntasFrecBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(48, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -206,6 +219,18 @@ public class Panel_pacientes extends javax.swing.JPanel {
         Panel_variante.repaint();
     }//GEN-LAST:event_historiaClinicaBtnActionPerformed
 
+    private void preguntasFrecBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_preguntasFrecBtnActionPerformed
+        // TODO add your handling code here:
+        VentanaHome.controlSesion.restart();
+        PreguntasFrecuentes PF = new PreguntasFrecuentes();
+        PF.setSize(1000, 1000);
+        PF.setLocation(0, 0);
+        Panel_variante.removeAll();
+        Panel_variante.add(PF, BorderLayout.CENTER);
+        Panel_variante.revalidate();
+        Panel_variante.repaint();
+    }//GEN-LAST:event_preguntasFrecBtnActionPerformed
+
     public void reset(){
         Panel_variante.removeAll();
         Panel_variante.add(B_citas);
@@ -223,6 +248,7 @@ public class Panel_pacientes extends javax.swing.JPanel {
     private javax.swing.JButton Directorio;
     private javax.swing.JPanel Panel_variante;
     private javax.swing.JButton historiaClinicaBtn;
+    private javax.swing.JButton preguntasFrecBtn;
     private javax.swing.JButton registrarFallaBtn;
     private javax.swing.JButton registroCitaButton;
     // End of variables declaration//GEN-END:variables
